bac4a2af756f43e0669f2dde2be1ec59d3a69f8f
Jibin <jibinlawrance@gmail.com>
2022-04-27 00:01:40 +0530

    add sign-in and sign-out page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require('../models/user')
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: "Profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    user: user
+                })
+            }
+
+            return res.redirect('/users/sign-in'); 
+        })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 // render the sign up page
@@ -43,5 +54,33 @@ module.exports.create = function(req,res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req,res){
-    //TODO later
+
+    //steps to authenticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err,user){
+        if(err){console.log('error in creating user in signing in'); return;}
+        // handle user found
+
+        if(user){
+            
+            // handle password which doesn't match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+            return res.redirect('back');
+        }
+    });
+
+}
+
+module.exports.signOut = function(req,res){
+    res.cookie('user_id',);
+    return res.redirect('/users/profile');
 }
