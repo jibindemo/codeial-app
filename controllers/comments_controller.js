76264e578c173418c7e767bfa53b4f1d795a74d3
Jibin <jibinlawrance@gmail.com>
2022-05-09 22:21:15 +0530

    add the link to delete a comment only with autherization

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,3 +1,4 @@
+const { redirect } = require('express/lib/response');
 const Comment = require('../models/commet');
 const Post = require('../models/post');
 
@@ -16,6 +17,23 @@ module.exports.create = function(req, res){
 
                 res.redirect('/');
             })
+        }
+    });
+}
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            
+            let postId = comment.post;
+            
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
         }
     });
 }
