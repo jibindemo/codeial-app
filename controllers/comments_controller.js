b4a3b29839790dd05bcaadf0e977e56f73641533
Jibin <jibinlawrance@gmail.com>
2022-05-11 23:47:23 +0530

    add flash message for comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -17,11 +17,12 @@ module.exports.create = async function(req, res){
             post.comments.push(comment);
             post.save();
     
+            req.flash('success', 'Comment added');
             res.redirect('/');
         }
     }catch(err){
-        console.log('Error', err);
-        return;
+        req.flash('error', err);
+        return res.redirect('/');
     }
 }
 
@@ -37,12 +38,14 @@ module.exports.destroy = async function(req,res){
             comment.remove();
     
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+            req.flash('success', 'Comment deleted');
             return res.redirect('back');
         }else{
             return res.redirect('back');
         }
     }catch(err){
-
+        req.flash('error', err);
+        return res.redirect('back');
     }
 
 }
