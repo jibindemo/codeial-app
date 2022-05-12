b9647612ca9b5e35209f9ac6c7d39c6faf0567cb
Jibin <jibinlawrance@gmail.com>
2022-05-12 09:31:09 +0530

    convert posts to ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,11 +3,20 @@ const Comment = require('../models/commet')
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
         
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
         req.flash('success', 'Post published');
         return res.redirect('back');
     }catch(err){
@@ -24,8 +33,17 @@ module.exports.destroy = async function(req,res){
         // .id means converting the object id into string
         if(post.user == req.user.id){
             post.remove();
-                    
+                  
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
 
             req.flash('success', 'Post and associated comments deleted');
 
